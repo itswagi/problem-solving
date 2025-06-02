ea92bee6c62cb7913b50bf752e12e97f710bba0a
Wajahat Zia <wzia.bscs16seecs@seecs.edu.pk>
2025-06-02 22:07:03 +0500

    updated 11

--- _learning/arrays/essentials/11_container_with_most_water/main.go
+++ _learning/arrays/essentials/11_container_with_most_water/main.go
@@ -1,33 +1,48 @@
-import "math"
+package main
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
 
 func maxArea(height []int) int {
-    max := 0
+    res := 0
 
     l:= 0
     r:= len(height) - 1
 
     for l < r {
-        area := math.Min(float64(height[l]), float64(height[r])) * float64(r-l)
-        max = int(math.Max(float64(max), area))
+        area := min(height[l], height[r]) * (r-l)
+        res = max(res, area)
         if height[l] < height[r] {
             l++
         } else {
             r--
         }
     }
-    return max
+    return res
 }
+
 
 // Brute Force
 // func maxArea(height []int) int {
-//     max := 0
+//     res := 0
 
 //     for i, h := range height {
 //         for j := i + 1; j < len(height); j++ {
-//             minHeight := math.Min(float64(h), float64(height[j]))
-//             area := (j - i) * int(minHeight)
-//             max = int(math.Max(float64(max), float64(area)))
+//             minHeight := min(h, height[j])
+//             area := (j - i) * minHeight
+//             res = max(res, area)
 //         }
 //     }
-//     return max
+//     return res
 // }
